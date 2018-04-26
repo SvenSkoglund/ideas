6a9ad87e8dc886ddd9c67792526aa4dcdcea2a07
Sven Skoglund <svenrskoglund@gmail.com>
2018-04-26 15:59:56 -0600

    pushing before db user change:pray:

---
+++ ideasMVC/src/test/java/ideasMVC/CommentTest.java
@@ -0,0 +1,38 @@
+package ideasMVC;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.skilldistillery.ideasjpa.entities.Comment;
+
+class CommentTest {
+
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private Comment comment;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		emf = Persistence.createEntityManagerFactory("ideas");
+		em = emf.createEntityManager();
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		em.close();
+		emf.close();
+	}
+	@Test
+	void test() {
+		comment = em.find(Comment.class, 1);
+		System.out.println(comment);
+	}
+
+}
