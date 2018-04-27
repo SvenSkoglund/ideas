60843e9e352e0514ac08f442ea278b24a7c79792
Sven Skoglund <svenrskoglund@gmail.com>
2018-04-27 15:14:55 -0600

    Tests are working :100:

--- ideasJPA/src/com/skilldistillery/ideas/data/IdeaDAO.java
+++ ideasJPA/src/com/skilldistillery/ideas/data/IdeaDAO.java
@@ -1,10 +1,14 @@
 package com.skilldistillery.ideas.data;
 
 import com.skilldistillery.ideasjpa.entities.Idea;
+import com.skilldistillery.ideasjpa.entities.IdeaLike;
+import com.skilldistillery.ideasjpa.entities.Profile;
 
 public interface IdeaDAO {
 
 	public boolean destroy(Idea idea);
 	public Idea update(Idea idea);
 	public Idea create(Idea idea);
+	IdeaLike createLike(Idea idea, Profile profile, Boolean vote);
+	IdeaLike updateLike(Idea idea, Profile profile, Boolean vote);
 }
--- ideasJPA/src/com/skilldistillery/ideasjpa/entities/Comment.java
+++ ideasJPA/src/com/skilldistillery/ideasjpa/entities/Comment.java
@@ -22,7 +22,7 @@ public class Comment {
 	
 	// Fields
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	@ManyToOne
