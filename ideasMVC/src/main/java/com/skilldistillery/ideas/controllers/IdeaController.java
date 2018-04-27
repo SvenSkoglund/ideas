e26702ff8b27f4457bc07c40a976af3cc9e13b6c
Dora Harper <doraeharper@gmail.com>
2018-04-27 12:19:14 -0600

    controller

--- ideasMVC/src/main/java/com/skilldistillery/ideas/controllers/IdeaController.java
+++ ideasMVC/src/main/java/com/skilldistillery/ideas/controllers/IdeaController.java
@@ -1,5 +1,42 @@
 package com.skilldistillery.ideas.controllers;
 
-public class IdeaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.skilldistillery.ideas.data.CommentDAO;
+import com.skilldistillery.ideas.data.IdeaDAO;
+import com.skilldistillery.ideas.data.ProfileDAO;
+import com.skilldistillery.ideas.data.UserDAO;
+
+@Controller
+public class IdeaController {
+	
+	@Autowired
+	private CommentDAO commentDao;
+	@Autowired
+	private IdeaDAO ideaDao;
+	@Autowired
+	private ProfileDAO profileDao;
+	@Autowired
+	private UserDAO userDao;
+
+	
+	// Home page
+	@RequestMapping(path="index.do", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+//		List<Idea> ideaList = ideaDao.something();//SOMETHING
+	//	mv.addObject("ideaList", ideaList);
+		mv.setViewName("WEB-INF/views/index.jsp");
+		
+		  return mv;
+	}
+	
+	
+	
+	
+	
 }
